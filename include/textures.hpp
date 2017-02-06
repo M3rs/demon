ed3f8087923beadd1cba9568876a1de6263396c1
Nathan Glynn <l33tkio@yahoo.com>
2017-02-05 18:33:56 -0600

    add Texture load/unload functions

--- include/textures.hpp
+++ include/textures.hpp
@@ -13,6 +13,10 @@ public:
   const sf::Texture& get(const std::string& filename);
   // Remove method?
 
+  void loadTexture(const std::string& filename);
+
+  void unloadTexture(const std::string& filename);
+
 private:
 
   std::map<std::string, const sf::Texture> m_cache;
--- src/textures.cpp
+++ src/textures.cpp
@@ -21,3 +21,16 @@ const sf::Texture &Textures::get(const std::string &filename) {
   m_cache.emplace(filename, std::move(tx));
   return m_cache[filename];
 }
+
+void Textures::loadTexture(const std::string& filename) {
+	sf::Texture texture;
+	if (!texture.loadFromFile(filename)) {
+		std::cout << "Error loading texture '" << filename << "'" << std::endl;
+		return;
+	}
+	m_cache.emplace(filename, texture);
+}
+
+void Textures::unloadTexture(const std::string& filename) {
+	m_cache.erase(filename);
+}
