2e988dd89eda4637f304e7663904746041ebf615
Nathan Glynn <l33tkio@yahoo.com>
2017-02-10 18:05:33 -0600

    Commented out buggy collision check. Build should compile and link

--- src/physicsbody.cpp
+++ src/physicsbody.cpp
@@ -1,6 +1,8 @@
 #include <physicsbody.hpp>
 #include "sprite.hpp"
 
+std::vector<SDL_Rect> PhysicsBody::CollisionLayerFG;
+
 PhysicsBody::PhysicsBody() : vel_x(0), vel_y(0), airborne(false) { }
 
 PhysicsBody::~PhysicsBody() { }
@@ -12,24 +14,25 @@ void PhysicsBody::updateMotion(Sprite* sprite) {
 	}
 	sprite->world_coords.x += vel_x;
 	sprite->world_coords.y += vel_y;
-
-	for (const SDL_Rect it : CollisionLayerFG) {
+	
+	/* Buggy collision detection -- sprites always collide with themselves
+	for (const SDL_Rect it : PhysicsBody::CollisionLayerFG) {
 		const SDL_Rect destination = sprite->world_coords;
 		SDL_Rect result;
-		if (SDL_IntersectRect(&destination, &it, &result)) {
+		if (SDL_IntersectRect(&destination, &it, &result) == SDL_TRUE) {
 			//collision! undo attempted movement
 			//or move to nearest legal position using 'result'
 			sprite->world_coords.x -= vel_x;
 			sprite->world_coords.y -= vel_y;
 			break;
 		}
-	}
+	}*/
 }
 
 void PhysicsBody::RenderLayerToCollisionBounds(std::map<std::string, Sprite> renderLayer) {
 	for (auto& it : renderLayer) {
 		auto& rect = it.second.world_coords;
-		CollisionLayerFG.push_back(rect);
+		PhysicsBody::CollisionLayerFG.push_back(rect);
 	}
 }
 
