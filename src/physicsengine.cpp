7564433fa76b3c902ed25eb5a26345ecd8555baf
Nathan Glynn <l33tkio@yahoo.com>
2017-02-15 15:16:04 -0600

    PhysicsEngine smarter collision - still need to change sprite on land, etc

--- src/physicsengine.cpp
+++ src/physicsengine.cpp
@@ -27,27 +27,52 @@ void PhysicsEngine::update() {
 			physBody->vel_y += 0.7F;
 		}
 
-		physBody->world_coords->x += physBody->vel_x;
-		physBody->world_coords->y += physBody->vel_y;
+		SDL_Rect projectedMovement = *physBody->world_coords;
+		projectedMovement.x += physBody->vel_x;
+		projectedMovement.y += physBody->vel_y;
 
+		bool collision_x(false), collision_y(false);
 		for (auto& kv_pair_colcheck : m_physicsList) {
 			auto physBody_colcheck = kv_pair_colcheck.second;
+			//TODO(nmg): 'walk off a cliff' transition to airborne
 			SDL_Rect result;
-			if (SDL_IntersectRect(physBody->world_coords,
+			if (SDL_IntersectRect(&projectedMovement,
 				physBody_colcheck->world_coords,
 				&result)
 				&& physBody->id != physBody_colcheck->id) {
 				//collision
-				physBody->world_coords->x -= physBody->vel_x;
-				physBody->world_coords->y -= physBody->vel_y;
+				if (physBody->airborne && physBody->vel_y > 0 && result.h > 0) {
+					//landing
+					physBody->airborne = false;
+					collision_y = true;
+					physBody->vel_y = 0;
+					physBody->world_coords->y = 
+						physBody_colcheck->world_coords->y - physBody->world_coords->h;
+				}
+				if (physBody->airborne && physBody->vel_y < 0 && result.h > 0) {
+					//head bumped something above
+					collision_y = true;
+					physBody->vel_y = 0;
+					physBody->world_coords->y =
+						physBody_colcheck->world_coords->y + physBody_colcheck->world_coords->h;
+				}
+				if (result.w > 0) {
+					collision_x = true;
+				}
 				break;
 			}
-		}		
+		}
+
+		if (!collision_x) {
+			physBody->world_coords->x += physBody->vel_x;
+		}
+		if (!collision_y) {
+			physBody->world_coords->y += physBody->vel_y;
+		}
+
 		//hack -- reset velx to 0 for next update
 		//do this in lua ideally
 		physBody->vel_x = 0;
 	}
-
-
 }
 
